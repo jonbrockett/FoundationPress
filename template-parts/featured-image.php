e30de7415f7c7eaaa4278ae2f6440a1f494aa30a
Jonathan Brockett <jonathanhbrockett@gmail.com>
2019-01-15 22:02:31 -0500

    Remove role="banner", it's already used in the header

--- template-parts/featured-image.php
+++ template-parts/featured-image.php
@@ -2,6 +2,6 @@
 // If a featured image is set, insert into layout and use Interchange
 // to select the optimal image size per named media query.
 if ( has_post_thumbnail( $post->ID ) ) : ?>
-	<header class="featured-hero" role="banner" data-interchange="[<?php the_thumbnail_bg( 'small' ); ?>, small], [<?php the_thumbnail_bg( 'medium' ); ?>, medium], [<?php the_thumbnail_bg( 'large' ); ?>, large], [<?php the_thumbnail_bg( 'xlarge' ); ?>, xlarge],[<?php the_thumbnail_bg( 'medium' ); ?>, small-retina], [<?php the_thumbnail_bg( 'large' ); ?>, medium-retina], [<?php the_thumbnail_bg( 'xlarge' ); ?>, large-retina], [<?php the_thumbnail_bg( 'full' ); ?>, xlarge-retina]">
+	<header class="featured-hero" data-interchange="[<?php the_thumbnail_bg( 'small' ); ?>, small], [<?php the_thumbnail_bg( 'medium' ); ?>, medium], [<?php the_thumbnail_bg( 'large' ); ?>, large], [<?php the_thumbnail_bg( 'xlarge' ); ?>, xlarge],[<?php the_thumbnail_bg( 'medium' ); ?>, small-retina], [<?php the_thumbnail_bg( 'large' ); ?>, medium-retina], [<?php the_thumbnail_bg( 'xlarge' ); ?>, large-retina], [<?php the_thumbnail_bg( 'full' ); ?>, xlarge-retina]">
 	</header>
 <?php endif;
