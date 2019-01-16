9aade7fa146217e48bfac07762f5f85039ecd3e5
Jonathan Brockett <jonathanhbrockett@gmail.com>
2019-01-16 09:55:07 -0500

    Fix <nav>'s and <section>'s

--- header.php
+++ header.php
@@ -44,33 +44,33 @@
 			 * Mobile Menu
 			 */
 			?>
-			<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
+			<section class="site-title-bar title-bar" arial-label="Mobile navigation" <?php foundationpress_title_bar_responsive_toggle(); ?>>
 				<div class="title-bar-left">
 					<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
 					<span class="site-mobile-title title-bar-title">
 						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 					</span>
 				</div>
-			</div>
+			</section>
 
 			<?php
 			/**
 			 * Desktop/Tablet Menu
 			 */
 			?>
-			<nav class="site-navigation top-bar" aria-label="Main menu">
+			<section class="site-navigation top-bar" aria-label="Site navigation">
 				<div class="top-bar-left">
 					<div class="site-desktop-title top-bar-title">
 						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 					</div>
 				</div>
-				<div id="menu" class="top-bar-right">
+				<nav id="menu" class="top-bar-right" aria-label="Main menu">
 					<?php foundationpress_top_bar_r(); ?>
 
 					<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
 						<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
 					<?php endif; ?>
-				</div>
-			</nav>
+				</nav>
+			</section>
 
 		</header>
