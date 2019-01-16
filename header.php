279cb7c163c276b774333ad8fd103ce11a103317
Jonathan Brockett <jonathanhbrockett@gmail.com>
2019-01-15 22:05:54 -0500

    Move skip links right after <body>

--- header.php
+++ header.php
@@ -18,60 +18,59 @@
 	</head>
 	<body <?php body_class(); ?>>
 
-	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
-		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
-	<?php endif; ?>
-
-	<?php
-	/**
-	 * Skip Links
-	 */
-	?>
-	<div class="skip-links" id="skip-links" role="navigation" aria-label="Skip links">
-		<a class="screen-reader-text" href="#main">Skip to content</a>
-		<a class="screen-reader-text" href="#menu">Skip to navigation</a>
-	</div>
-
-
-	<?php
-	/**
-	 * Site Header
-	 */
-	?>
-	<header id="header" class="site-header" role="banner">
-
 		<?php
 		/**
-		 * Mobile Menu
+		 * Skip Links
 		 */
 		?>
-		<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
-			<div class="title-bar-left">
-				<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
-				<span class="site-mobile-title title-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</span>
-			</div>
-		</div>
-
-		<?php
-		/**
-		 * Desktop/Tablet Menu
-		 */
-		?>
-		<nav class="site-navigation top-bar" aria-label="Main menu">
-			<div class="top-bar-left">
-				<div class="site-desktop-title top-bar-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</div>
-			</div>
-			<div id="menu" class="top-bar-right">
-				<?php foundationpress_top_bar_r(); ?>
-
-				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
-					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
-				<?php endif; ?>
-			</div>
+		<nav id="skip-links" class="skip-links" aria-label="Skip links">
+			<a class="screen-reader-text" href="#main">Skip to content</a>
+			<a class="screen-reader-text" href="#menu">Skip to navigation</a>
 		</nav>
 
-	</header>
+		<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
+			<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
+		<?php endif; ?>
+
+		<?php
+		/**
+		 * Site Header
+		 */
+		?>
+		<header id="header" class="site-header" role="banner">
+
+			<?php
+			/**
+			 * Mobile Menu
+			 */
+			?>
+			<div class="site-title-bar title-bar" <?php foundationpress_title_bar_responsive_toggle(); ?>>
+				<div class="title-bar-left">
+					<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
+					<span class="site-mobile-title title-bar-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					</span>
+				</div>
+			</div>
+
+			<?php
+			/**
+			 * Desktop/Tablet Menu
+			 */
+			?>
+			<nav class="site-navigation top-bar" aria-label="Main menu">
+				<div class="top-bar-left">
+					<div class="site-desktop-title top-bar-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					</div>
+				</div>
+				<div id="menu" class="top-bar-right">
+					<?php foundationpress_top_bar_r(); ?>
+
+					<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
+						<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
+					<?php endif; ?>
+				</div>
+			</nav>
+
+		</header>
