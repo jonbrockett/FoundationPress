7dd494ebc24826b59e7b27c4f6a09fe95366b366
Jon Brockett <geckomist@gmail.com>
2019-01-20 17:01:49 -0500

    Add skip links, sticky header, better layout mobile nav

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 		 * Skip Links
 		 */
 		?>
-		<nav id="skip-links" class="skip-links" aria-label="Skip links">
+		<nav id="skip-links" class="skip-links" aria-label="Skip links" data-smooth-scroll>
 			<a class="screen-reader-text" href="#main">Skip to content</a>
 			<a class="screen-reader-text" href="#menu">Skip to navigation</a>
 		</nav>
@@ -32,45 +32,60 @@
 			<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
 		<?php endif; ?>
 
+
+		<?php // Front top frame
+		if ( is_page_template( 'page-templates/front.php' ) ) {
+			echo '<div class="top-frame grid-frame grid-y">';
+		}
+		?>
+
 		<?php
 		/**
 		 * Site Header
 		 */
 		?>
-		<header id="header" class="site-header">
+		<div class="header-sticky-container" data-sticky-container>
+			<header id="header" class="site-header sticky" data-sticky data-options="marginTop:0;">
 
-			<?php
-			/**
-			 * Mobile Menu
-			 */
-			?>
-			<section class="site-title-bar title-bar" arial-label="Mobile navigation" <?php foundationpress_title_bar_responsive_toggle(); ?>>
-				<div class="title-bar-left">
-					<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
-					<span class="site-mobile-title title-bar-title">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-					</span>
-				</div>
-			</section>
-
-			<?php
-			/**
-			 * Desktop/Tablet Menu
-			 */
-			?>
-			<section class="site-navigation top-bar" aria-label="Site navigation">
-				<div class="top-bar-left">
-					<div class="site-desktop-title top-bar-title">
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+				<?php
+				/**
+				 * Mobile Menu
+				 */
+				?>
+				<section class="site-title-bar title-bar" arial-label="Mobile navigation" <?php foundationpress_title_bar_responsive_toggle(); ?> data-hide-for="large">
+					<div class="title-bar-left">
+						<div class="grid-x grid-padding-x">
+							<div class="cell auto">
+								<span class="site-mobile-title title-bar-title">
+									<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+								</span>
+							</div>
+							<div class="cell shrink">
+								<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
+							</div>
+						</div>
 					</div>
-				</div>
-				<nav id="menu" class="top-bar-right" aria-label="Main menu">
-					<?php foundationpress_top_bar_r(); ?>
+				</section>
 
-					<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
-						<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
-					<?php endif; ?>
-				</nav>
-			</section>
+				<?php
+				/**
+				 * Desktop/Tablet Menu
+				 */
+				?>
+				<section class="site-navigation top-bar" aria-label="Site navigation">
+					<div class="top-bar-left">
+						<div class="site-desktop-title top-bar-title">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+						</div>
+					</div>
+					<nav id="menu" class="top-bar-right" aria-label="Main menu">
+						<?php foundationpress_top_bar_r(); ?>
 
-		</header>
+						<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
+							<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
+						<?php endif; ?>
+					</nav>
+				</section>
+
+			</header>
+		</div>
