0e7a48d7a24b2947ad08133ec044127b20197c96
Jon Brockett <geckomist@gmail.com>
2019-01-20 17:00:47 -0500

    Layout copyright and privacy menu

--- footer.php
+++ footer.php
@@ -15,12 +15,17 @@
  */
 ?>
 <footer id="footer" class="footer">
-  <div class="grid-container">
-    <div class="grid-x">
-      <div class="cell">
-        <nav aria-label="Privacy menu">
-          <?php foundationpress_privacy_nav(); ?>
-        </nav>
+  <div class="section-spacing">
+    <div class="grid-container">
+      <div class="grid-x grid-padding-x grid-padding-y align-center-middle">
+        <div class="cell medium-shrink medium-order-2">
+          <nav aria-label="Privacy menu">
+            <?php foundationpress_privacy_nav(); ?>
+          </nav>
+        </div>
+        <div class="cell medium-auto medium-offset-1 text-center medium-order-1">
+          &copy; <?php echo date('Y', strtotime("+3 months")); ?> <?php bloginfo('name'); ?>
+        </div>
       </div>
     </div>
   </div>
